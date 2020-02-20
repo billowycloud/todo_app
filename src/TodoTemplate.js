1c2af1431113ca93037d4504099d7f90fe062dee
billowycloud <54979977+billowycloud@users.noreply.github.com>
2020-02-20 17:08:41 +0900

    Performance optimization | 2-2Step: Using useReducer

--- src/TodoTemplate.js
+++ src/TodoTemplate.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useReducer, useRef, useCallback } from "react";
 import styled from "styled-components";
 import TodoInsert from "TodoInsert";
 import TodoList from "TodoList";
@@ -29,11 +29,36 @@ function createBulkTodos() {
   return array;
 }
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case "INSERT": //새로 추가
+      //{type: 'INSERT', todo: {id: 1, text: 'todo', checked: false}}
+      return todos.concat(action.todo);
+    case "REMOVE": //제거
+      //{type: 'REMOVE', id: 1}
+      return todos.filter(todo => todo.id !== action.id);
+    case "TOGGLE": //토글
+      //{type: 'TOGGLE', id: 1}
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+}
+/*
+  2-2: useReducer 사용
+*/
+
 const TodoTemplate = () => {
-  const [todos, setTodos] = useState(createBulkTodos); /* Big Data */
+  const [todos, dispatch] = useReducer(
+    todoReducer,
+    undefined,
+    createBulkTodos
+  ); /* Big Data */
 
   /*id값은 렌더링되는 정보가 아니기도하고, 이 값이 바뀐다고 컴포넌트가 리렌더링될 필요도 없다. */
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
   /* props로 전달해야 할 함수를 만들 시, useCallback을 사용해 컴포넌트 성능을 아낄 수 있다. */
   const onInsert = useCallback(text => {
     const todo = {
@@ -41,27 +66,18 @@ const TodoTemplate = () => {
       text,
       checked: false
     };
-    setTodos(todos => todos.concat(todo));
+    dispatch({ type: "INSERT", todo });
     nextId.current += 1;
   }, []);
 
   const onRemove = useCallback(id => {
     /* id를 받아와서 같은 아이디일경우 삭제 */
-    setTodos(todos => todos.filter(todo => todo.id !== id));
+    dispatch({ type: "REMOVE", id });
   }, []);
 
   const onToggle = useCallback(id => {
-    setTodos(todos =>
-      todos.map(todo =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      )
-    );
+    dispatch({ type: "TOGGLE", id });
   }, []);
-
-  /*
-  [2-1: UseState의 함수형 업데이트]
-  setTodos를 사용할때 그 안에 todos =>를 추가한 후 useCallback 두번째 파라미터내용을 지운다. 
-  */
 
   return (
     <>
