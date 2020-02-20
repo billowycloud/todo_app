3db47fec50c02356070deb359cf830a717cbee9e
billowycloud <54979977+billowycloud@users.noreply.github.com>
2020-02-20 16:55:11 +0900

    Performance optimization | 2Step: Using useState

--- src/TodoTemplate.js
+++ src/TodoTemplate.js
@@ -21,47 +21,47 @@ const Title = styled.div`
   justify-content: center;
 `;
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({ id: 1, text: `할 일 ${i}`, checked: false });
+  }
+  return array;
+}
+
 const TodoTemplate = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "밥먹자", checked: true },
-    { id: 2, text: "씻자", checked: true },
-    { id: 3, text: "놀자", checked: false }
-  ]);
+  const [todos, setTodos] = useState(createBulkTodos); /* Big Data */
 
   /*id값은 렌더링되는 정보가 아니기도하고, 이 값이 바뀐다고 컴포넌트가 리렌더링될 필요도 없다. */
   const nextId = useRef(4);
   /* props로 전달해야 할 함수를 만들 시, useCallback을 사용해 컴포넌트 성능을 아낄 수 있다. */
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos]
-  );
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false
+    };
+    setTodos(todos => todos.concat(todo));
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    id => {
-      /* id를 받아와서 같은 아이디일경우 삭제 */
-      setTodos(todos.filter(todo => todo.id !== id));
-    },
-    [todos]
-  );
+  const onRemove = useCallback(id => {
+    /* id를 받아와서 같은 아이디일경우 삭제 */
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo
-        )
-      );
-    },
-    [todos]
-  );
+  const onToggle = useCallback(id => {
+    setTodos(todos =>
+      todos.map(todo =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  }, []);
+
+  /*
+  [2-1: UseState의 함수형 업데이트]
+  setTodos를 사용할때 그 안에 todos =>를 추가한 후 useCallback 두번째 파라미터내용을 지운다. 
+  */
 
   return (
     <>
